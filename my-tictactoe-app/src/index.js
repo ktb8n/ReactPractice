8690020af4d566d0c5f619a9b8d36810864eedb2
Katie Baeten <katherinebaeten@gmail.com>
2022-08-09 12:26:35 -0500

    Edit: the board component assigns each square a value,
    it is not yet rendered.

--- my-tictactoe-app/src/index.js
+++ my-tictactoe-app/src/index.js
@@ -11,7 +11,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
 	renderSquare(i) {
-		return <Square value={i} />;
+		return <Square value={i}  />;
 	}
 
 	render() {
